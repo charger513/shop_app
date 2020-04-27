f2d78086d4a8fe2951d4aa92841c6dca901a3b53
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-27 12:02:59 -0500

    Displaying a List of Cart Items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart';
@@ -42,7 +43,20 @@ class CartScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final cartItem = cart.items.values.toList()[index];
+
+                  return CartItem(id: cartItem.id, price: cartItem.price, quantity: cartItem.quantity, title: cartItem.title);
+                },
+              ),
+            ),
           ],
         ));
   }
