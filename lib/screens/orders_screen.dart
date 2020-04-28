2744a7c4a739c19195c85d5f8126eb541a0c7d54
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-27 21:48:57 -0500

    Using a Side Drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
+
+  static const String routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -12,6 +16,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (BuildContext context, int index) {
