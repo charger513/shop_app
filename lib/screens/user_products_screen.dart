01beef436a3d2b5fbea3f8c059862a3c28040eb6
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-29 14:43:12 -0500

    Implementing Pull-to-Refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop_app/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const String routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -25,26 +29,29 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (BuildContext context, int index) {
-            final product = productsData.items[index];
-            return Column(
-              children: <Widget>[
-                UserProductItem(
-                  id: product.id,
-                  title: product.title,
-                  imageUrl: product.imageUrl,
-                ),
-                Divider(
-                  color: Colors.grey,
-                  height: 20,
-                ),
-              ],
-            );
-          },
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (BuildContext context, int index) {
+              final product = productsData.items[index];
+              return Column(
+                children: <Widget>[
+                  UserProductItem(
+                    id: product.id,
+                    title: product.title,
+                    imageUrl: product.imageUrl,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    height: 20,
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
