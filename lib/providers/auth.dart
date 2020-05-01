6bd54df9fb535b29b1146a8c6429fe73bc5a9351
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-01 13:42:10 -0500

    VSCODE tiene un bug que

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,37 +2,44 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authenticate(String email, String password, String urlSegment) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDTmvgQzaOTh1BToB-COA0XsNq1fRqtD-k';
 
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
 
-    print(json.decode(response.body));
+      final responseData = json.decode(response.body);
+
+      if(responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error; // VS Code tiene un error que marca esto como unhandled. La línea de abajo lo soluciona (No pasa en Android Studio)
+      // return Future.error(error);
+    }
   }
 
   Future<void> signup(String email, String password) async {
-
     return _authenticate(email, password, 'signUp');
-
   }
 
   Future<void> login(String email, String password) async {
-
     return _authenticate(email, password, 'signInWithPassword');
-
   }
 }
